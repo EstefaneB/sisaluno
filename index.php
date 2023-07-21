2643348b08f6c4f7c83b4895c1d7352acb968643
EstéfaneBenevides <130498776+EstefaneB@users.noreply.github.com>
2023-07-20 22:12:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style type="text/css">
+    .conteiner{
+        width: 100%;
+        height: 90vh;
+        background-color: #1D2951;
+        display: flex;
+        justify-content: center;  
+    }
+    .box{
+        
+       
+        display: flex;  
+        flex-direction: column;
+        align-items: center;
+        /*justify-items: center;*/      
+        justify-content: center;
+    }
+    .box button{
+        width: 300px;
+        
+        display: inline-block;
+        margin-top:30px;
+        background-color:  #4CAF50;
+        border: none;
+        color: white;
+        text-align: center;
+        padding: 14px 20px;
+        text-decoration: none;
+        display: block;
+        font-size: 16px;
+        margin: 10px;
+        cursor: pointer;
+        border-radius: 5px;
+        
+        
+
+    }
+    
+    
+
+    .button a {
+      color: white;
+      text-decoration: none;
+    }
+
+    .button:hover {
+      background-color:  #5a43;
+    }
+
+</style>
+<body>
+    
+
+    <div class="header">
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #ffd55a; height:13VH;" >
+          <div class="container-fluid">
+              <a class="navbar-brand" href="#">SISTEMA ALUNO </a>
+          </div>
+    </nav>
+    </div>
+    <div class="conteiner">
+
+    <div class="box">
+        <button class="button"><a href="cadaluno.php">CADASTRAR</a></button>
+        <button class="button"><a href="listaalunos.php">LISTA</a></button>
+        </div>
+        
+       
+    </div>
+
+
+    </div>
+    
+    
+
+    
+  
+
+</body>
+</html>
